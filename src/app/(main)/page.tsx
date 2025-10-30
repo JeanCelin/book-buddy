f431dedce006c11db5e77b1791208482f7cf2436
Jean Celin <jean_estudos@hotmail.com>
2025-10-30 14:42:56 -0300

    created first version of form

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import styles from "./page.module.css";
 import Header from "./(main-componentes)/Header";
 import SearchBar from "@/components/SearchBar";
 import SearchButtons from "@/components/SearchButtons";
+import { useState } from "react";
+
+import AddBookForm from "@/components/AddBookForm";
 
 export default function Home() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -16,6 +22,11 @@ export default function Home() {
         <div className={styles.section}>
           <SearchButtons />
         </div>
+        {showForm && (
+          <div className={`${styles.section} ${styles.form}`}>
+            <AddBookForm />
+          </div>
+        )}
       </main>
     </div>
   );
---
+++ src/components/AddBookForm.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useState } from "react";
+
+import styles from "./AddBookForm.module.css";
+import Button from "./Button";
+
+export default function AddBookForm() {
+  const [genre, setGenre] = useState("romance");
+  const [status, setStatus] = useState("Quero Ler");
+
+  return (
+    <form className={styles.form}>
+      <div>
+        <span>Adicionar Livro</span>
+        <p>Adicione um novo livro à sua biblioteca</p>
+      </div>
+      <div className={styles.fields__container}>
+        <div className={styles.field}>
+          <label htmlFor="title">Título *</label>
+          <input id="title" type="text"></input>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="author">Autor *</label>
+          <input id="author" type="text"></input>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="genre">Gênero</label>
+          <select id="genre" name="genre" defaultValue="romance">
+            <option value="autoajuda">Autoajuda</option>
+            <option value="autobiografia">Autobiografia</option>
+            <option value="aventura">Aventura</option>
+            <option value="biografia">Biografia</option>
+            <option value="distopia">Distopia</option>
+            <option value="drama">Drama</option>
+            <option value="educacao">Educação</option>
+            <option value="ensaios">Ensaios</option>
+            <option value="fantasia">Fantasia</option>
+            <option value="ficcao-cientifica">Ficção Científica</option>
+            <option value="filosofia">Filosofia</option>
+            <option value="historia">História</option>
+            <option value="hq">HQ / Graphic Novel</option>
+            <option value="infantil">Infantil</option>
+            <option value="juvenil">Juvenil</option>
+            <option value="misterio">Mistério</option>
+            <option value="negocios">Negócios</option>
+            <option value="poesia">Poesia</option>
+            <option value="psicologia">Psicologia</option>
+            <option value="religiao">Religião</option>
+            <option value="romance">Romance</option>
+            <option value="suspense">Suspense</option>
+            <option value="terror">Terror</option>
+          </select>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="status">Status *</label>
+          <select id="status" defaultValue="Quero ler">
+            <option value="Quero Ler">Quero Ler</option>
+            <option value="Lendo">Lendo</option>
+            <option value="Lido">Lido</option>
+          </select>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="cover">URL da Capa</label>
+          <input id="cover" type="url" placeholder="https://"></input>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="rating">Avaliação (1-5)</label>
+          <input type="number" min={0} max={5}></input>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="notes">Notas</label>
+          <textarea
+            id="notes"
+            placeholder="Suas anoteções sobre o livro..."></textarea>
+        </div>
+      </div>
+      <div className={styles.buttons__container}>
+        <Button background="var(--foreground)" color="var(--background)" text="Cancelar"/>
+        <Button background="var(--background) " color="var(--foreground)" text="Enviar"/>
+      </div>
+    </form>
+  );
+}
